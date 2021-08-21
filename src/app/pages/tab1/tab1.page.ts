86119f25f754f3b5834c1f80eaf357028c22a20d
brayangomez08 <gomezmancobrayanalexander@gmail.com>
2021-08-20 19:47:49 -0500

    Alertcontroller - Add a list to our service

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 import { WishesService } from 'src/app/services/wishes.service';
 
 @Component({
@@ -8,9 +9,45 @@ import { WishesService } from 'src/app/services/wishes.service';
   styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
-  constructor(public wishesService: WishesService, private router: Router) {}
+  constructor(
+    public wishesService: WishesService,
+    private router: Router,
+    public alertController: AlertController
+  ) {}
 
-  addList() {
-    this.router.navigateByUrl('/tabs/tab1/add');
+  async addList() {
+    const alert = await this.alertController.create({
+      header: 'New list',
+      inputs: [
+        {
+          name: 'title',
+          type: 'text',
+          placeholder: 'List name',
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('cancel');
+          },
+        },
+        {
+          text: 'Create',
+          handler: (data) => {
+            console.log(data);
+            if (data.title.length === 0) {
+              return;
+            }
+
+            this.wishesService.createList(data.title);
+          },
+        },
+      ],
+    });
+
+    alert.present();
+    //this.router.navigateByUrl('/tabs/tab1/add');
   }
 }
--- src/app/services/wishes.service.ts
+++ src/app/services/wishes.service.ts
@@ -14,4 +14,10 @@ export class WishesService {
 
     this.lists.push(list1, list2);
   }
+
+  createList(title: string) {
+    const newList = new List(title);
+
+    this.lists.push(newList);
+  }
 }
